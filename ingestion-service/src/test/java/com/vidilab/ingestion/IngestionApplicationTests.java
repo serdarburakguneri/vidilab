4a4308a3350ecebf4bbf4e3813db8f4ae75d0475
sburakguneri <burakos@serdars-MacBook-Pro.local>
2024-08-21 20:09:06 +0300

    feat: ingestion service added

---
+++ ingestion-service/src/test/java/com/vidilab/ingestion/IngestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidilab.ingestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngestionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
